b07f5812faf6fdfeb94a3bf67ffbcc50572bedbe
hj222hi <hj222hi@student.lnu.se>
2016-11-03 00:59:59 +0100

    hej

--- giter.sh
+++ giter.sh
@@ -10,7 +10,24 @@ else
   git push origin master:$branch
 fi
 
-day=$(date +%u)
 
-say "your code is pushed now master, have a nice $day"
+day=$(date +%u)
+if [ "${day}" -eq 1 ]
+then
+dayText="monday"
+else if ["${day}" -eq 2]; then
+dayText="tuesday"
+else if [["${day}" -eq 3]; then
+  dayText="wensday"
+else if ["${day}" -eq 4]; then
+  dayText="thursday"
+else if ["${day}" -eq 5]; then
+  dayText="friday"
+else if ["${day}" -eq 6]; then
+  dayText="saturday"
+else if ["${day}" -eq 7]; then
+  dayText="sunday"
+fi
+
+say "your code is pushed now master, have a nice $dayText"
 echo basename `git rev-parse --show-toplevel`
