4c4c3e76ee5c4d78b6043bd52d186e2e0788b850
hj222hi <hj222hi@student.lnu.se>
2016-11-03 00:47:22 +0100

    hej

--- giter.sh
+++ giter.sh
@@ -13,20 +13,7 @@ then
 git push origin master:$branch
 fi
 
-if ! fatal
-then
-    say "Error you dumb count"
-    echo >&2 message
-    if [ $branch == null ]
-    then
-        git push origin master
-    fi
 
-    if [ $branch != null ]
-    then
-    git push origin master:$branch
-    fi
-fi
 
 say "your code is pushed now master, have a nice day"
 echo basename `git rev-parse --show-toplevel`
