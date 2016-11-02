5a8f166738cecfd524dbe286622a168c827bb1d1
hj222hi <hj222hi@student.lnu.se>
2016-11-03 00:41:57 +0100

    hej

--- giter.sh
+++ giter.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -e
 commit_message="$1"
+branch="$2"
 git add . -A
 git commit -m "$commit_message"
-git push origin master
+if [ $branch == null]
+then
+    git push origin master
+fi
+
+if [ $branch != null ]
+then
+git push origin $branch
+fi
+
 say "your code is pushed now master, have a nice day"
 echo basename `git rev-parse --show-toplevel`
